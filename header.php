297cfa5d578d0ecdffebeb9003d3b682f23ed5aa
Sergio Ralandison <sergioralandison@outlook.com>
2020-06-03 14:12:17 +0200

    projet blueasy pixel perfect

---
+++ header.php
@@ -0,0 +1,323 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" media="mediatype and|not|only (expressions)" href="screen.css">
+    <title>BLUEASY</title>
+</head>
+
+<body>
+    <header>
+
+			<!--NAVBAR part-->
+
+				<nav class="navbar">
+					<div class="container p-0">
+						<div class="row text-white d-flex align-items-center w-100 mx-auto">
+							<h1 class="col-7">
+								BLU<span class="text-info blueasy">E</span>ASY
+							</h1>
+							<div class="col-1 text-info text2 text-center">
+								HOME
+							</div>
+							<div class="col-1">
+								SERVICES
+							</div>
+							<div class="col-1">
+								FEATURES
+							</div>
+							<div class="col-1">
+								PORTFOLIO
+							</div>
+							<div class="col-1">
+								CONTACT
+							</div>
+						</div>
+					</div>
+				</nav>
+
+				<!--HERO part-->
+
+				<section class="container mx-auto p-0 py-5">
+					<h1 class="d-flex justify-content-center"><mark class="text-white">"I'M LOOKING FOR THE UNEXPECTED.</mark></h1>
+					<h1 class="d-flex justify-content-center"><mark class="text-white">I'M LOOKING FOR THINGS I'VE NEVER SEEN BEFORE."</mark></h1>
+				</section>
+		</header>
+
+		<!--SERVICES part-->
+
+		<main>
+			<section class="container mx-auto text-white">
+
+				<!--Title section-->
+
+				<div class="row pt-3">
+					<h2 class="pt-4"><span id="border-bottom-services">SER</span>VICES</h2>
+				</div>
+
+				<!--Columns-->
+
+				<div class="row">
+					<div class="col-3 p-0">
+						<img src="images/icons/coffee.png" alt="" class="pt-4 pb-3">
+						<h3>COFFEE</h3>
+						<p>Apple pie icing sweet. Brownie jelly-o applicake<br />
+							applicake sweet roll liquorice bear claw. Jujubes<br />
+							carrot cake cotton candy sweet tart brownie.<br />
+							Tiramisu applicake jujubes.</p>
+					</div>
+					<div class="col-3 p-0">
+						<img src="images/icons/instant.png" alt="" class="pt-4 pb-3">
+						<h3>INSTANT</h3>
+						<p>Apple pie icing sweet. Brownie jelly-o applicake<br />
+							applicake sweet roll liquorice bear claw. Jujubes<br />
+							carrot cake cotton candy sweet tart brownie.<br />
+							Tiramisu applicake jujubes.</p>
+					</div>
+					<div class="col-3 p-0">
+						<img src="images/icons/serious.png" alt="" class="pt-4 pb-3">
+						<h3>SERIOUS</h3>
+						<p>Apple pie icing sweet. Brownie jelly-o applicake<br />
+							applicake sweet roll liquorice bear claw. Jujubes<br />
+							carrot cake cotton candy sweet tart brownie.<br />
+							Tiramisu applicake jujubes.</p>
+					</div>
+					<div class="col-3 p-0">
+						<img src="images/icons/frame.png" alt="" class="pt-4 pb-3">
+						<h3>FRAME</h3>
+						<p>Apple pie icing sweet. Brownie jelly-o applicake<br />
+							applicake sweet roll liquorice bear claw. Jujubes<br />
+							carrot cake cotton candy sweet tart brownie.<br />
+							Tiramisu applicake jujubes.</p>
+					</div>
+				</div>
+			</section>
+
+			<!--PORTFOLIO part-->
+
+			<section class="pt-4">
+				<div class="container p-0">
+					<div class="row pt-4 mt-3 d-flex align-items-center mx-auto">
+
+						<!--Title portfolio-->
+
+						<h2 class="col-7 p-0">
+							<span id="border-bottom-portfolio">POR</span>TFOLIO
+						</h2>
+
+						<!--Filters-->
+
+						<div class="col-auto text-info text-center text2">
+							ALL
+						</div>
+						<div class="col-auto">
+							PANORAMAS
+						</div>
+						<div class="col-auto">
+							PORTRAITS
+						</div>
+						<div class="col-auto">
+							MACRO
+						</div>
+						<div class="col-auto">
+							JOURNAL
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<!--GALLERY part-->
+
+			<section class="container p-0 pt-4 mt-2" id="portfolio">
+				<div class="row">
+      		<figure class="col-3 overlay">
+          	<img alt="picture" src="images/gallery/photo1.jpg" class="img-fluid gallery-img">
+      		</figure>
+					<figure class="col-3">
+          	<img alt="picture" src="images/gallery/photo2.jpg" class="img-fluid gallery-img">
+      		</figure>
+					<figure class="col-3">
+          	<img alt="picture" src="images/gallery/photo3.jpg" class="img-fluid gallery-img">
+      		</figure>
+					<figure class="col-3">
+          	<img alt="picture" src="images/gallery/photo4.jpg" class="img-fluid gallery-img">
+      		</figure>
+				</div>
+				<div class="row">
+      		<figure class="col-3">
+          	<img alt="picture" src="images/gallery/photo5.jpg" class="img-fluid gallery-img">
+      		</figure>
+					<figure class="col-3">
+          	<img alt="picture" src="images/gallery/photo6.jpg" class="img-fluid gallery-img">
+      		</figure>
+					<figure class="col-3">
+          	<img alt="picture" src="images/gallery/photo7.jpg" class="img-fluid gallery-img">
+      		</figure>
+					<figure class="col-3">
+          	<img alt="picture" src="images/gallery/photo8.jpg" class="img-fluid gallery-img">
+      		</figure>
+				</div>
+			</section>
+
+			<!--JUST DEFAULT SECTION part-->
+
+			<section class="container-fluid mt-3" id="just-default-section">
+				<div class="container pt-3 p-0">
+					<div class="row pt-3">
+						<h2 class="text-white col-auto pt-4 mr-1"><span id="border-bottom-section">JUS</span>T DEFAULT SECTION</h2>*
+					</div>
+					<div class="row">
+						<div class="text-white col-6 pt-4">
+							<p class="size-text2">Bear claw marzipan bear claw applicake I love muffin. Lemon drops gummi<br/>
+              bears pastry gummi bears sesame snaps I love unerdwear.com.Soufflé<br/>
+              cotton candy dessert candy ice cream wafer gummies cheesecake brownie.</p>
+
+							<p class="size-text2">Muffin chupa chups jelly beans sweet pie applicake. Croissant chocolate<br/>
+							cake I love pudding. Ice cream I love powder pudding apple pie<br/>
+							marshmallow. Cupcake marzipan oat cake bonbon I love candy canes toffee.</p>
+							<button type="button" class="btn btn-outline-dark col-2 mt-2">VISIT ME</button>
+						</div>
+						<div class="col-6" id="vimeo-video">
+              <iframe src="https://player.vimeo.com/video/196697984?title=0&byline=0&portrait=0" width="554" height="312" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+					</div>
+				</div>
+			</section>
+
+			<!--Twitter part-->
+
+			<section class="container-fluid" id="twitter-section">
+				<div class="container p-0">
+					<div class="row">
+						<h2 class="col-1 pt-4 p-3 text-white"><span id="border-bottom-twitter">TW</span>ITTER</h2>
+					</div>
+          <div class="d-flex justify-content-end">
+            <img src="images/icons/twitter.png" id="twitter-icon" class="position-relative"/>
+          </div>
+					<div class="col-11 p-0 text-white">
+						<p class="size-text3">Unerdwear cookie liquorice. Cake donut cupcake lollipop soufflé candy. Chocolate oat cake</p>
+						<p class="size-text3"><span class="font-weight-bold">@cheesecake</span> tootsie roll.</p>
+					</div>
+				</div>
+			</section>
+
+      <!--WHO IS JOHN DOE ? part-->
+
+      <section class="container-fluid p-0" id="bg-johndoe">
+        <div class="container p-0">
+          <div class="row">
+						<h2 class="col-4 pt-4 mt-3 text-white"><span id="border-bottom-johndoe">WH</span>O IS JOHN DOE?</h2>
+					</div>
+          <div class="row">
+            <div class="col-6 text-white pt-4 mt-2">
+              <p class="size-text2">Bear claw marzipan bear claw applicake I love muffin. Lemon drops gummi bears pastry<br/>
+              gummi bears sesame snaps I love unerdwear.com. Soufflé cotton candy dessert candy ice<br/>
+              cream wafer gummies cheesecake brownie.</p>
+
+              <p class="size-text2">Muffin chupa chups jelly beans sweet pie applicake. Croissant chocolate cake I love pudding.<br/>
+              Ice cream I love powder pudding apple pie marshmallow. Cupcake marzipan oat cake<br/>
+              bonbon I love candy canes toffee.</p>
+            </div>
+            <div class="col-6 text-white pt-4 mt-2">
+              <p class="size-text2">Pudding dessert jujubes tiramisu gingerbread croissant tiramisu applicake. Sesame snaps<br/>
+              sugar plum cotton candy chocolate bonbon lemon drops candy canes cotton candy. Cake<br/>
+              toffee pie bear claw pastry.</p>
+              <img src="images/icons/twitter-white.png" alt="picture" class="border-img m-2">
+              <img src="images/icons/google-plus.png" alt="picture" class="border-img m-2">
+              <img src="images/icons/dribble.png" alt="picture" class="border-img m-2">
+              <img src="images/icons/coffee-white.png" alt="picture" class="border-img m-2">
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!--TESTIMONIALS part-->
+
+      <section class="container-fluid p-0" id="testimonials">
+        <div class="container p-0">
+          <div class="row">
+            <h2 class="col-3 pt-3 mt-3"><span id="border-bottom-testimonials">TES</span>TIMONIALS</h2>
+          </div>
+          <div class="d-flex justify-content-end">
+            <img src="images/icons/guillemets.png" id="guillemets-icon" class="position-relative"/>
+          </div>
+          <div class="col-11 p-0">
+            <p class="size-text3">Applicake chocolate cake wafer toffee pie soufflé wafer. Tart marshmallow wafer macaroon</p>
+            <p class="size-text3">cheesecake jelly. Gingerbread cookie soufflé sweet roll sweet roll jelly-o.</p>
+            <p class="size-text2 text-primary font-weight-bold">- Walter White</p>
+          </div>
+        </div>
+      </section>
+
+      <!--CONTACT part-->
+
+      <section class="container-fluid p-0" id="contact">
+        <div class="container p-0 text-white">
+          <div class="row">
+            <h2 class="col-5 pt-4 mt-3"><span id="border-bottom-contact">CO</span>NTACT</h2>
+          </div>
+          <form class="pt-4">
+            <div class="form-row">
+              <div class="form-group col-5">
+                <label for="inputEmail4">FULL NAME</label>
+                <input type="text" class="form-control transparent-input rounded-0" id="inputEmail4">
+              </div>
+              <div class="pt-3 mt-2">
+                <p class="size-text4">Jujubes bear claw croissant wafer sweet lemon drops brownie chocolate bar. Sweet<br/>
+                lollipop halvah tootsie roll cake fruitcake. Chocolate cake chocolate bar gingerbread<br/>
+                lollipop tart. Jujubes bear claw donut chupa chups</p>
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="form-group col-5">
+                <label for="inputAddress">EMAIL</label>
+                <input type="text" class="form-control transparent-input rounded-0" id="inputAddress">
+              </div>
+              <div class="pt-3 mt-2">
+                <p class="size-text4">Linda Newman<br/>
+                W325 State Road 123<br/>
+                Mondovi, WI(Wisconsin) 98746-54321<br/>
+                (715) 946 - 1234</p>
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="form-group col-12 mt-2">
+                <label for="exampleFormControlTextarea1">MESSAGE</label>
+                <textarea class="form-control transparent-textarea rounded-0" type="text" id="exampleFormControlTextarea1" rows="7"></textarea>
+              </div>
+            </div>
+            <div class="pt-3">
+              <button type="submit" class="btn btn-info col-4 rounded-0">MESSAGE</button>
+            </div>
+          </form>
+        </div>
+      </section>
+
+      <!--FOOTER part-->
+
+      <footer class="container-fluid p-0" id="footer">
+        <div class="container pt-2 p-0 text-secondary">
+          <div class="row">
+            <div class="pt-1 mt-3 col-auto bdr-right">
+						  HOME
+						</div>
+						<div class="pt-1 mt-3 col-auto bdr-right">
+						  SERVICES
+						</div>
+						<div class="pt-1 mt-3 col-auto bdr-right">
+							PORTFOLIO
+						</div>
+						<div class="pt-1 mt-3 col-8">
+							CONTACT
+						</div>
+            <div class="pt-1 mt-3 padding-left">
+              2013 ©
+            </div>
+          </div>
+        </div>
+      </footer>
+		</main>
